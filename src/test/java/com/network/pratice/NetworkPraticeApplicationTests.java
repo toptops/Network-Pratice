2ce6a05fcf5649ab486c84f70e8224f9c2f84303
toptops <seongtab@naver.com>
2019-11-23 03:03:23 +0900

    [20191123] 네트워크 프로그래밍 프로젝트 생성

---
+++ src/test/java/com/network/pratice/NetworkPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.network.pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
